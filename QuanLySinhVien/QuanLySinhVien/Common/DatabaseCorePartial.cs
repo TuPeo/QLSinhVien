4447f226d51cae28da568b84f7b823d0fc2b4ddd
levanhung93 <levanhung.cntt.93@gmail.com>
2015-03-18 22:40:55 +0700

    sua file common

---
+++ QuanLySinhVien/QuanLySinhVien/Common/DatabaseCorePartial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace QuanLySinhVien.Common
+{
+   partial class DatabaseCore
+    {
+        #region Method 
+        /// <summary>
+        /// thực hiện câu lệnh tạo các bảng dữ liệu
+        /// </summary>
+        private static void ExecuteCreateDatabase()
+        {
+
+        }
+        #endregion Method
+    }
+}
